b6aee2468e0d01cf8ecf11bcc543c9549cded2e2
root <36671276+vk1996@users.noreply.github.com>
2020-01-26 13:47:16 +0530

    Apache Copyright added

--- fullgradV2.py
+++ fullgradV2.py
@@ -1,5 +1,5 @@
 '''
-Copyright 2019 Vignesh Kotteeswaran <iamvk888@gmail.com>
+Copyright 2020 Vignesh Kotteeswaran <iamvk888@gmail.com>
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
